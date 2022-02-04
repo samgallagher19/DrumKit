98c2d49fecbf4300da78f410ab51d2c9bb867d7c
sgallagher <samuel.gallagher@cengage.com>
2022-02-04 09:44:11 -0500

    Adding event listener to document for keyboard presses

--- index.js
+++ index.js
@@ -1,15 +1,59 @@
-
 var buttons = document.querySelectorAll(".drum");
 
-for(var i = 0; i < buttons.length; i++) {
-  buttons[i].addEventListener("click", playTom1Audio);
+for (var i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", function() {
+    var buttonInnerHTML = this.innerHTML;
+    drumSound(buttonInnerHTML);
+});
 }
 
-function handleClick() {
-  alert("I got clicked!");
-}
 
-function playTom1Audio() {
-  var tom1Audio = new Audio('sounds/tom-1.mp3');
-  tom1Audio.play();
+document.addEventListener("keydown", function(event) {
+  drumSound(event.key);
+});
+
+
+
+function drumSound(key) {
+  switch (key) {
+    case "w":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+
+    case "a":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+
+    case "s":
+      var tom2 = new Audio("sounds/tom-1.mp3");
+      tom2.play();
+      break;
+
+    case "d":
+      var kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+      break;
+
+    case "j":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+
+    case "k":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+
+    case "l":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+
+
+    default: console.log(key);
+
+  }
+
 }
